60d7398c1f9ee7a3cb489a01e09919e3dbe0ab16
efim <efim.krutihin@yandex.ru>
2023-05-22 19:27:02 +0300

    add min in task one

--- Задание 1/Program.cs
+++ Задание 1/Program.cs
@@ -1,11 +1,16 @@
 ﻿int a = 5;
 int b = 7;
 int max = 0;
+int min = 0;
 
 if( a > b ){
     max = a;
+    min = b;
 } else{
     max = b;
+    min = a;
 }
 Console.Write("max = ");
-Console.Write(max);
+Console.WriteLine(max);
+Console.Write("min =");
+Console.Write(min);
