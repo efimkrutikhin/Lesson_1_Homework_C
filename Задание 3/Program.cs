3f42e83b8a4c2868848ce5f54b9cda32ef633ba8
efim <efim.krutihin@yandex.ru>
2023-05-22 20:16:47 +0300

    задача 6 выполнена

--- Задание 3/Program.cs
+++ Задание 3/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿Console.WriteLine("Type your number: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+if(numA % 2 == 0){
+    Console.WriteLine("Число чётное");
+}else{
+    Console.WriteLine("Число нечётное");
+}
